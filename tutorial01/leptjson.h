ea147923a157d047f07bde48eda3b3aee580557f
JnKDog <530661401@qq.com>
2021-09-07 11:02:07 +0100

    t1 ok

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -3,10 +3,12 @@
 
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
 
+// tree struct
 typedef struct {
     lept_type type;
 }lept_value;
 
+// result statues
 enum {
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
@@ -14,6 +16,7 @@ enum {
     LEPT_PARSE_ROOT_NOT_SINGULAR
 };
 
+// parse 
 int lept_parse(lept_value* v, const char* json);
 
 lept_type lept_get_type(const lept_value* v);
